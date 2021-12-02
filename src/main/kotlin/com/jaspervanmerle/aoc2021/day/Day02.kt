aae92123a40b2b15af07fa157bc09f4d33ed6b1e
Jasper van Merle <jaspervmerle@gmail.com>
2021-12-02 11:29:25 +0100

    Solve day 2

---
+++ src/main/kotlin/com/jaspervanmerle/aoc2021/day/Day02.kt
@@ -0,0 +1,42 @@
+package com.jaspervanmerle.aoc2021.day
+
+class Day02 : Day("1804520", "1971095320") {
+    private val commands = getInput()
+        .lines()
+        .map { it.split(" ") }
+        .map { it[0] to it[1].toInt() }
+
+    override fun solvePartOne(): Any {
+        var position = 0
+        var depth = 0
+
+        for ((type, x) in commands) {
+            when (type) {
+                "forward" -> position += x
+                "down" -> depth += x
+                "up" -> depth -= x
+            }
+        }
+
+        return position * depth
+    }
+
+    override fun solvePartTwo(): Any {
+        var aim = 0
+        var position = 0
+        var depth = 0
+
+        for ((type, x) in commands) {
+            when (type) {
+                "down" -> aim += x
+                "up" -> aim -= x
+                "forward" -> {
+                    position += x
+                    depth += aim * x
+                }
+            }
+        }
+
+        return position * depth
+    }
+}
