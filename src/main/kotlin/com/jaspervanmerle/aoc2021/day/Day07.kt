bee99650ddb1291081e703afe72d6a2c6a538333
Jasper van Merle <jaspervmerle@gmail.com>
2021-12-07 11:09:19 +0100

    Solve day 7

---
+++ src/main/kotlin/com/jaspervanmerle/aoc2021/day/Day07.kt
@@ -0,0 +1,23 @@
+package com.jaspervanmerle.aoc2021.day
+
+import kotlin.math.abs
+
+class Day07 : Day("356179", "99788435") {
+    private val positions = getInput()
+        .split(",")
+        .map { it.toInt() }
+
+    override fun solvePartOne(): Any {
+        return getMinimalFuelToAlign { it }
+    }
+
+    override fun solvePartTwo(): Any {
+        return getMinimalFuelToAlign { (it * (it + 1)) / 2 }
+    }
+
+    private fun getMinimalFuelToAlign(distanceToFuel: (distance: Int) -> Int): Int {
+        return (positions.minOf { it }..positions.maxOf { it }).minOf { neutral ->
+            positions.sumOf { distanceToFuel(abs(neutral - it)) }
+        }
+    }
+}
